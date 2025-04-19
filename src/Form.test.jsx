7b57105e6a6830ad49682200d5737bcb180945e5
Emiri Sato <emirisato.1003@gmail.com>
2025-04-19 10:25:13 -0700

    #

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -1,1 +1,11 @@
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { describe } from "vitest";
+import Form from "./Form";
+
+describe('Form Component', () => {
+    it('should disable calculate button when operand is empty', async() => {
+        const user = userEvent.setup()
+        render(<Form/>)
+    })
+})
