19fc0f8b522a888fa34a7cbb0bf486b591115e3c
Emiri Sato <emirisato.1003@gmail.com>
2025-04-20 11:24:49 -0700

    create a test file 'form.test.jsx.

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -4,8 +4,18 @@ import { describe } from "vitest";
 import Form from "./Form";
 
 describe('Form Component', () => {
-    it('should disable calculate button when operand is empty', async() => {
+    it('should disable submit button when operand is empty', async() => {
         const user = userEvent.setup()
         render(<Form/>)
+
+        const operandInput = screen.getByLabelText(/calculate with:/i)
+        const submitButton = screen.getByRole('button', {name: /submit/i })
+        expect(submitButton).toBeDisabled()
+
+        await user.type(operandInput, '41')
+
+        await user.clear(operandInput)
+
+        expect(calculateButton).toBeDisabled()
     })
 })
